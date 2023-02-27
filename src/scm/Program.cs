bf076097c45b90224905a6e967e1ca457773d264
Michael C. Tiberio <github@tiberio.org>
2023-02-27 12:45:00 -0800

    Adding an index page that dynamically loads data

--- src/scm/Program.cs
+++ src/scm/Program.cs
@@ -1,6 +1,18 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseHttpsRedirection();
+
+app.UseDefaultFiles();
+
+app.UseStaticFiles(
+    new StaticFileOptions
+    {
+        DefaultContentType = "text/html; charset=utf-8"
+    });
+
+app.MapGet(
+    "api/stuff",
+    () => """[{"left":"top and left","right":"top and right"},{"left":"bottom left yo","right":"last one out!!"},{"left":"wait, waht now?","right":"THERE ARE THREE OF THEM?!?!"}]""");
 
 app.Run();
