b175608bedb6c8fb14e3e28fce66ddb34a5da527
Michael C. Tiberio <github@tiberio.org>
2023-03-02 14:21:41 -0800

    Some basic create and delete functionality

--- src/scm/Api.cs
+++ src/scm/Api.cs
@@ -8,11 +8,14 @@ internal static class ScmApi
         public required string Right { get; init; }
     }
 
-    public static IEnumerable<Thing> GetThings()
-    {
-        yield return new () { Left = "da left", Right = "da right" };
-        yield return new () { Left = "mo left", Right = "mo right" };
-        yield return new () { Left = "even mo left!!", Right = "even mo right!!" };
-        yield return new () { Left = "required stuff here", Right = "required stuff here too" };
-    }
+    private static List<Thing> allTheThings = new ();
+
+    public static IEnumerable<Thing> GetThings() =>
+        allTheThings;
+
+    public static void CreateThing(Thing thing) =>
+        allTheThings.Add(thing);
+
+    public static void DeleteThing(Thing thing) =>
+        allTheThings.Remove(thing);
 }
--- src/scm/Program.cs
+++ src/scm/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Mvc;
 using SimpleContactManager.Api;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,5 +9,23 @@ app.UseDefaultFiles();
 app.UseStaticFiles();
 
 app.MapGet("api/things", ScmApi.GetThings);
+
+app.MapPost(
+    "api/things",
+    (HttpRequest httpRequest) =>
+    {
+        ScmApi.CreateThing(
+            new ScmApi.Thing()
+            {
+                Left = httpRequest.Form["Left"].ToString(),
+                Right = httpRequest.Form["Right"].ToString()
+            });
+        return Results.Redirect("/");
+    });
+
+app.MapDelete(
+    "api/things",
+    ([FromBody] ScmApi.Thing thing) =>
+        ScmApi.DeleteThing(thing));
 
 app.Run();
